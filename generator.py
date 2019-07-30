ea56b945a107e74e20d7c8dd8cddf6f4641804e5
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-07-30 16:56:40 -0500

    new way to map things

--- generator.py
+++ generator.py
@@ -6,6 +6,26 @@ from PIL import Image
 import psf 
 from scipy.signal import convolve as conv2
 # from scipy.signal import deconvolve
+import cv2
+from scipy import misc
+
+def plot_spectrum(img):
+    from matplotlib.colors import LogNorm
+    # A logarithmic colormap
+    plt.imshow(np.abs(img), norm=LogNorm(vmin=5))
+    plt.colorbar()
+    plt.figure()
+    plot_spectrum(img)
+    plt.title('log View')
+
+def histogram(im):
+    vals = im.mean(axis=2).flatten()
+    # calculate hstogram
+    counts, bins = np.histogram(vals, range(257))
+    # plot histogram centered on values 0..255
+    plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
+    plt.xlim([-0.5, 255.5])
+    plt.show()
 
 def generate_psf(x, y, cmap='hot', savebin=False, savetif=True, savevol=False,
                 plot=True, **kwargs):
@@ -13,14 +33,14 @@ def generate_psf(x, y, cmap='hot', savebin=False, savetif=True, savevol=False,
 
     args = {
         'shape': (x, y),  # number of samples in z and r direction
-        'dims': (5.0, 5.0),   # size in z and r direction in micrometers
+        'dims': (10.0, 10.0),   # size in z and r direction in micrometers
         'ex_wavelen': 488.0,  # excitation wavelength in nanometers
         'em_wavelen': 520.0,  # emission wavelength in nanometers
         'num_aperture': 1.2,
         'refr_index': 1.333,
         'magnification': 1.0,
         'pinhole_radius': 0.05,  # in micrometers
-        'pinhole_shape': 'square',
+        'pinhole_shape': 'round',
     }
     args.update(kwargs)
 
@@ -46,9 +66,11 @@ def generate_psf(x, y, cmap='hot', savebin=False, savetif=True, savevol=False,
 #     return fftpack.fftshift(fftpack.ifftn(fftpack.ifftshift(star_fft/psf_fft)))
 
 
-img = Image.open('elepant.png')
-img = img.convert('L', dither=Image.NONE)
-img = np.float32(img)
+img = Image.open('image.jpg')
+# img = img.convert('L', dither=Image.NONE)
+img = misc.face(gray=True)
+print(img.dtype)
+# img = np.float32(img)
 psf = generate_psf(img.shape[0],img.shape[1])
 convimg = conv2(img,psf)
 img = convimg
@@ -56,8 +78,8 @@ img = convimg
 # if recover == img:
 #     print("hell yeah")
 img += np.random.poisson(img,img.shape)
-img = gaussian_filter(img,sigma=2)
+img = gaussian_filter(img,sigma=8)
+# img = np.uint8(img)
 
-
-plt.imshow(img)
+plt.imshow(img,cmap='gray')
 plt.show()
